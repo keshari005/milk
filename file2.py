25b0728f8398f60feffe2c974df40b655352d9cc
Vishal Keshari <vishalkeshari05@gmail.com>
2026-04-28 13:50:31 +0530

    first commit

---
+++ file2.py
@@ -0,0 +1,7 @@
+print("Hello Vishal")
+print("Welcome to Python")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Sum is:", a + b*2)
---
+++ file3.py
@@ -0,0 +1,110 @@
+# name= input("Enter your name: ")
+# age= int(input("Enter your age: "))
+
+# print("Hello", name, "you are", age, "years old")
+
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+
+# print(f"Hello {name}, you are {age} years old")
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+
+# print(f"Hello {name}, you are {age} years old Next year you will be {age + 1} years old")
+
+# name = input("Enter your name: ")
+# # Sal = 500000
+# Sal = float(input("Enter your salary: "))
+# print(f"Hello {name}, your salary is {Sal} per annum")
+
+# name="Vishal"
+# print(f"Hello {name}, welcome to Python programming")
+
+# a=5
+# b=10
+# print("Sum is:", a + b)
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# print("Sum is:", a + b)
+
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print(f"You are eligible to vote and you are {age} years old")
+# else:
+#     print(f"You are not eligible to vote and you are {age} years old")
+
+# a= int(input("Enter first number: "))
+# b= int(input("Enter second number: "))
+# print("Sum is:", a + b, "Difference is:", a - b, "Product is:", a * b, "Quotient is:", a / b)   
+
+# num = int(input("Enter number: "))
+
+# if num % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+# num = int(input("Enter number: "))
+# if num % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+# num = int(input("Enter number: "))
+# if num >80:
+#     print("Print A grade")
+# elif num > 60:
+#     print("Print B grade")
+# elif num > 40:  
+#     print("Print C grade")
+# else:    print("Print D grade")
+
+# num = int(input("Enter number:"))
+# for i in range(5):
+#     print(i)
+
+#dd
+# Write a single-line comment
+
+# Comment out this line so it does not run:
+# print("This should not run")
+
+# Add a multiline comment
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+# x = str(3)    # x will be '3'
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
+
+
+# x = "Sally" # x is now of type str
+# x = 4       # x is of type int
+# print(x)
+
+# print(x)        # 3
+# print(type(x))  # <class 'str'>
+# x = 5
+# y = "John"
+# print(type(x))
+# print(type(y))
+
+# Create an integer
+# x = 5
+
+# # Create a float
+# y = 3.14
+
+# # Create a complex number
+# z = 2+3
+
+# # Print the types
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
